25cee976ff840086d3f5ada08be7a1727f28079c
Yashas V <130334248+yashas8197@users.noreply.github.com>
2024-04-25 06:39:31 +0000

    added styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import useFetch from "./useFetch";
 
 export default function App() {
@@ -7,14 +8,14 @@ export default function App() {
   );
   console.log(data);
   return (
-    <div>
+    <div className="container my-4">
       {loading && <p>Loading...</p>}
       {error && <p>Something went wrong</p>}
       <h1>Todo Lists</h1>
-      <ul>
+      <ul className="list-group">
         {data &&
           data.map((todo) => (
-            <li>
+            <li className="list-group-item">
               <p>
                 <strong>{todo.title}</strong>
               </p>
